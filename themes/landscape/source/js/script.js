f124106f852c48cf89a5666a916fd72f347bcf8b
Alex Bogdanovski <alex@erudika.com>
2016-12-09 22:33:16 +0200

    fixed fb comments width

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -76,4 +76,46 @@
 		$('.fancybox').fancybox();
 	}
 
+	////////////////////////////
+	//	SEARCH BOX
+	////////////////////////////
+
+	var APPID = "app:albogdano";
+	var ENDPOINT = "https://paraio.com/v1"; // OR "http://localhost:8080/v1"
+	$.ajaxSetup({
+		headers: {'Authorization': 'Anonymous ' + APPID}
+	});
+
+	var blogposts = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		remote: {
+			url: ENDPOINT + '/blogposts?q=%QUERY',
+			wildcard: '%QUERY',
+			transform: function (res) {
+				return res.items || [];
+			}
+		}
+	});
+
+	$('#search-box').typeahead({
+		hint: false,
+		highlight: true,
+		minLength: 3
+	},
+	{
+		name: 'blogposts',
+		source: blogposts,
+		templates: {
+			notFound: '<i>No results.</i>'
+		},
+		display: function (result) {
+			return result.name;
+		}
+	});
+
+	$('#search-box').bind('typeahead:select', function (ev, result) {
+		window.location = result.url || '';
+	});
+
 })(jQuery);
