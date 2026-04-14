e504cdff839b11956a4deb3dce4f02aff6567c4f
Alex Bogdanovski <alex@erudika.com>
2026-04-14 19:44:15 +0300

    updated robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,6 +3,8 @@ import type { APIRoute } from 'astro';
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
+Disallow: /cdn-cgi/
+Disallow: /blog/archives/
 
 Sitemap: ${sitemapURL.href}
 `;
